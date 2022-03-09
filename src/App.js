02a94ad5ce2ffdde006b1e93b72def4dc7dca245
Dipak 2056 <paudelsantosh508@gmail.com>
2022-03-09 12:23:44 +1100

    added the new statte, use-effect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Clock from "./Clock";
 
 function App() {
@@ -7,6 +7,34 @@ function App() {
   const [timerHours, setTimerHours] = useState();
   const [timerMinutes, setTimerMinutes] = useState();
   const [timerSeconds, setTimerSeconds] = useState();
+
+  let interval;
+  const startTimer = () => {
+    const countDownDate = new Date("March 28,2022").getTime();
+    interval = setInterval(() => {
+      const now = new Date().getTime();
+      const duration = countDownDate - now;
+      const days = Math.floor(duration / (24 * 60 * 60 * 1000));
+      const hours = Math.floor(
+        (duration % (24 * 60 * 60 * 1000)) / (1000 * 60 * 60)
+      );
+      const minutes = Math.floor((duration % (60 * 60 * 1000)) / (1000 * 60));
+      const seconds = Math.floor((duration % (60 * 1000)) / 1000);
+
+      if (duration < 0) {
+        //stop timer
+        clearInterval(interval.current);
+      } else {
+        setTimerDays(days);
+        setTimerHours(hours);
+        setTimerMinutes(minutes);
+        setTimerSeconds(seconds);
+      }
+    });
+  };
+  useEffect(() => {
+    startTimer();
+  });
   return (
     <div className="App">
       <Clock
